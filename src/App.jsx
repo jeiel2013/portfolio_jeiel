9a538d856d29523099e6ce4d5feaf0dacdc8eb9b
jeiel2013 <contatojeiel2013@gmail.com>
2026-07-12 21:48:14 -0300

    feat: App.jsx atualizado com todas as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Links from "./pages/Links";
 import Home from "./pages/Home";
+import Servicos from "./pages/Servicos";
+import Projetos from "./pages/Projetos";
+import CaseStudy from "./pages/CaseStudy";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/servicos" element={<Servicos />} />
+        <Route path="/projetos" element={<Projetos />} />
+        <Route path="/projetos/:slug" element={<CaseStudy />} />
         <Route path="/links" element={<Links />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
